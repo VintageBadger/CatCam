9f858787c99d261b17029b1f946951061f9a3bc6
VintageBadger <sleepypandas2@gmail.com>
2019-12-18 10:47:14 -0600

    Initial Commit

---
+++ videoStream.py
@@ -0,0 +1,13 @@
+# workon <env_name> # my environment is named py3cv4
+# pip install opencv-contrib-python
+# pip install zmq
+# pip install imutils
+
+# cd ~
+# git clone https://github.com/jeffbass/imagezmq.git
+# Live video streaming over network with OpenCV and ImageZMQShell
+# cd ~/.virtualenvs/py3cv4/lib/python3.6/site-packages
+# ln -s ~/imagezmq/imagezmq imagezmq
+
+# cd ~/.virtualenvs/py3cv4/lib/python3.6/site-packages
+# ln -s ~/imagezmq/imagezmq imagezmq
