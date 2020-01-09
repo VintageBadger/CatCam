e56bfe80e7001714be8dd55353b67eb9491c9d7a
VintageBadger <sleepypandas2@gmail.com>
2020-01-09 14:23:44 -0600

    Video stream and exit

--- videoStream.py
+++ videoStream.py
@@ -1,13 +1,28 @@
-# workon <env_name> # my environment is named py3cv4
-# pip install opencv-contrib-python
-# pip install zmq
-# pip install imutils
+from imutils.video import VideoStream
+import argparse
+import numpy as np
+import cv2
 
-# cd ~
-# git clone https://github.com/jeffbass/imagezmq.git
-# Live video streaming over network with OpenCV and ImageZMQShell
-# cd ~/.virtualenvs/py3cv4/lib/python3.6/site-packages
-# ln -s ~/imagezmq/imagezmq imagezmq
+cap = cv2.VideoCapture(0)
 
-# cd ~/.virtualenvs/py3cv4/lib/python3.6/site-packages
-# ln -s ~/imagezmq/imagezmq imagezmq
+while(True):
+    # Capture frame-by-frame
+    # read() Grabs, decodes and returns the next video frame
+    # if camera disconnected or no more frames in video, returns false and empty image
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    # cvtColor(src, dst, code = color space conversion, dstCn = num of channels in dst) 
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # cie = cv2.cvtColor(frame, cv2.COLOR_BGR2XYZ) #oranges turn blue
+    # YCrCb = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb) #lots of yellows and pinks
+    # No color transformations are needed, but they are fun :)
+
+    # Display the resulting frame
+    cv2.imshow('CatCam', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
